444a6ac72f24f854472fcd4eb001edae2f2c2800
tomcomtang <tomcomtang@gmail.com>
2025-12-08 16:59:18 +0800

    Remove package-lock and ignore it

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: edgeoneAdapter(),
+  output: 'server',
   integrations: [react()]
 });
